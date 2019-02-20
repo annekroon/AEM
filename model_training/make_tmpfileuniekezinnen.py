43d9f48ee062d1f02fafc48fe57937a9672b2a46
annekroon <annekroon@gmail.com>
2019-02-20 12:40:15 +0100

    changed final configuration

--- model_training/make_tmpfileuniekezinnen.py
+++ model_training/make_tmpfileuniekezinnen.py
@@ -3,23 +3,16 @@ import inca
 import gensim
 import logging
 import re
+from collections import defaultdict
+import json
 
 lettersanddotsonly = re.compile(r'[^a-zA-Z\.]')
 
 PATH = "/home/anne/tmpanne/"
-#outlets = ['telegraaf (print)', 'nrc (print)', 'volkskrant (print)', 'ad (print)', 'trouw (print)']
-outlets = ['telegraaf (print)', 'nrc (print)', 'volkskrant (print)', 'ad (print)', 'trouw (print)',  'telegraaf (www)', 'nrc (www)', 'volkskrant (www)', 'ad (www)', 'trouw (www)', 'nu' , 'nos']
 
-w2v_params = {
-    'alpha': 0.025,
-    'size': 100,
-   # 'window': 15,
-   # 'iter': 5,
-    'min_count': 5,
-    'sg': 1,
-    'hs': 0,
-    'negative': 5
-}
+outlets = ['ad (print)' , 'ad (www)' , 'anp' ,  'bd (www)' , 'bndestem (www)' , 'destentor (www)' , 'ed (www)' , 'fd (print)' , 'frieschdagblad (www)' , 'gelderlander (www)' , 'metro (print)' , 'metro (www)' ,  'nos' , 'nos (www)' , 'nrc (print)' , 'nrc (www)' , 'nu' , 'parool (www)' , 'pzc (www)' ,'spits (www)' , 'telegraaf (print)' , 'telegraaf (www)'  , 'trouw (print)' , 'trouw (www)' , 'tubantia (www)' , 'volkskrant (print)' , 'volkskrant (www)' , 'zwartewaterkrant (www)' ]
+
+
 
 def preprocess(s):
     s = s.lower().replace('!','.').replace('?','.')  # replace ! and ? by . for splitting sentences
@@ -32,32 +25,20 @@ class train_model():
         self.doctype = doctype
         self.fromdate = fromdate
         self.todate = todate
-        if type(self.doctype) is str:
-            self.query = {
-                  "query": {
-                          "bool": {
-                                    "filter": [
-                                                { "match": { "doctype": self.doctype}},
-                                                { "range": { "publication_date": { "gte": self.fromdate, "lt":self.todate }}}
-                                              ]
-                                  }
-                        }
-                }
-        elif type(self.doctype) is list:
-            self.query = {
-                  "query": {
-                          "bool": {
-                                    "filter": [ {'bool': {'should': [{ "match": { "doctype": d}} for d in self.doctype]}},
-                                                { "range": { "publication_date": { "gte": self.fromdate, "lt":self.todate }}}
-                                              ]
-                                  }
-                        }
-                }
-
-
-        self.documents = 0
+        self.numberofarticles = defaultdict(int)
+        self.numberoffailedarticles = defaultdict(int)
         self.failed_document_reads = 0
-        self.model = gensim.models.Word2Vec(**w2v_params)
+        self.documents = 0
+        if type(self.doctype) is list:
+            self.query = {
+                  "query": {
+                          "bool": {
+                                    "filter": [ {'bool': {'should': [{ "term": { "doctype": d}} for d in self.doctype]}},
+                                                { "range": { "publication_date": { "gte": self.fromdate, "lt":self.todate }}}
+                                              ]
+                                  }
+                        }
+                }
 
         self.sentences = set()
         for sentence in self.get_sentences():
@@ -67,17 +48,21 @@ class train_model():
 
     def get_sentences(self):
         for d in inca.core.database.scroll_query(self.query):
+            self.numberofarticles[d['_source']['doctype']] +=1
             try:
                 self.documents += 1
                 sentences_as_lists = (s.split() for s in preprocess(d['_source']['text']).split('.'))
                 for sentence in sentences_as_lists:
                     yield sentence
-            except:
+            except Exception as e:
+                # print(e)
+                logger.debug(d['_source']['doctype'])
+                self.numberoffailedarticles[d['_source']['doctype']] +=1
                 self.failed_document_reads +=1
                 continue
 
 def train_and_save(fromdate,todate,doctype):
-    filename = "{}uniekezinnen_{}_{}".format(PATH,fromdate,todate)
+    filename = "{}uniekezinnen_testshort_{}_{}".format(PATH,fromdate,todate)
 
     casus = train_model(doctype,fromdate,todate)
 
@@ -85,6 +70,8 @@ def train_and_save(fromdate,todate,doctype):
         for sentence in casus.sentences:
             fo.write(sentence)
             fo.write('\n')
+    with open(filename+'_numberofarticles.json', mode = 'w') as fo:
+        json.dump({"succes":casus.numberofarticles, "fail":casus.numberoffailedarticles}, fo)
     
     print('Created file with sentences: {}'.format(filename))
 
@@ -95,4 +82,4 @@ if __name__ == "__main__":
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s')
     logging.root.setLevel(level=logging.INFO)
 
-    train_and_save(fromdate = "2000-01-01", todate = "2016-12-31", doctype = outlets)
+    train_and_save(fromdate = "2017-12-25", todate = "2017-12-31", doctype = outlets)
